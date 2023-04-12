08ce048896e48b14acc9dcd421fcb87e53db24d2
abhishek.patel10 <abhishek.patel@yudiz.com>
2023-04-12 11:50:45 +0530

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+#if you are deploying to https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:abhiishek-10/skillshow-abhishek.git main:gh-pages
+
+cd -
