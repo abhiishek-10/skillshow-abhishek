ed244b2bc673f5557178221fb11a7da66ece9c47
abhishek.patel10 <abhishek.patel@yudiz.com>
2023-04-17 11:53:50 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/skillshow-abhishek/',
+  base: '/skillshow-abhishek/dist/',
   plugins: [react()],
 
 })
